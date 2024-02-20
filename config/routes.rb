805893f6fbbc1dc3a99ebdd7bc9f6719cea98cfb
ghadeer kherullah <kherullahghadeer@gmx.com>
2024-02-20 16:52:45 +0100

    Rails Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # Create: one user story with 2 actions: get + post
+  # create a get action for the create new task
+  # get "tasks/new", to: "tasks#new", as: :new_task
+
+  # # create a post action for the create new task
+  # post "tasks", to: "tasks#create"
+
+  # # Read all tasks
+  # get "tasks", to: "tasks#index"
+
+  # # Read one task
+  # get "tasks/:id", to: "tasks#show", as: :task
+
+  # # update: one user story with 2 actions: get + patch
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+
+  # # delete
+  # delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks
 end
